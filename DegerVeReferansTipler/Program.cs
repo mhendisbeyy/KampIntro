2f288213540193ca2fe6a9fde52cee1f88b97620
Ali ATALAY <mhendisbey@gmail.com>
2021-03-06 00:26:45 +0300

    6 Mart Ders sonu(16 Ocak) Ali ATALAY

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -21,6 +21,8 @@ namespace DegerVeReferansTipler
             //sayilar1[0]=?  999
             Console.WriteLine(sayilar1[0]);
 
+            Console.WriteLine("-----------------------------");
+
 
         }
     }
