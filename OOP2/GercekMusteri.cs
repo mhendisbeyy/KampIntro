423d10faced471d75290a52399b282c063f49520
example <mhendisbey@gmail.com>
2021-03-11 02:36:38 +0300

    İnheritance- 11.03.2021

---
+++ OOP2/GercekMusteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Indiviual-Bireysel
+    //Miras-İnheritance
+    class GercekMusteri:Musteri
+    {
+       
+        public string TCNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Ali";
+            musteri1.Soyadi = "ATALAY";
+            musteri1.TCNo = "01234567890";
+
+            //Nur-Hack Yazılım
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "544";
+            musteri2.SirketAdi = "Nur-Hack Yazılım";
+            musteri2.VergiNo = "0123456789";
+
+
+            //Gerçek - Tüzel Müşteri 
+            //SOLİD
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+            musteriManager.Ekle(musteri4);
+        
+        }
+    }
+}
